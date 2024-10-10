38979e652995f91a171c240cc0fc0490b3c3bc16
Erick bueno <erickjb93@gmail.com>
2024-10-10 15:09:47 -0300

    feat(User Entity): add user entity

--- src/elf/src/app/components/user/user.repository.ts
+++ src/elf/src/app/components/user/user.repository.ts
@@ -1,12 +1,21 @@
 import {createState, createStore, Store, withProps} from '@ngneat/elf';
+import {withEntities} from "@ngneat/elf-entities"
 
 interface UserProps{
+  id:number | null;
   name: string;
   email: string;
 }
-
+interface User {
+  id: number;
+  name: string;
+  email: string;
+}
+interface User{}
+//criando o estado a ser utilizado na nossa store
 const {state, config} = createState(
-  withProps<UserProps>({name:'',email:''})
+  withProps<UserProps>({name:'',email:'', id:null}), //setando os valores iniciais dos campos
+  withEntities()
 )
 
 const userStore = new Store({name: "user", state, config})
