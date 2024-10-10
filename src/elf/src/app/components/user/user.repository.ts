6fadc0ab05f6bdf2e5a20d27b3e2ba93fffe201b
Erick bueno <erickjb93@gmail.com>
2024-10-10 16:12:58 -0300

    feat(add props in state):

--- src/elf/src/app/components/user/user.repository.ts
+++ src/elf/src/app/components/user/user.repository.ts
@@ -1,23 +1,34 @@
 import {createState, createStore, Store, withProps} from '@ngneat/elf';
-import {withEntities} from "@ngneat/elf-entities"
+import {entitiesPropsFactory, withEntities} from "@ngneat/elf-entities"
 
-interface UserProps{
-  id:number | null;
-  name: string;
-  email: string;
-}
 interface User {
   id: number;
   name: string;
   email: string;
+  address: Address['id']
 }
+interface Address {
+  id: number;
+  street: string;
+  city: string;
+  state: string;
+  zip: string;
+}
+interface GlobalProps{
+  accessToken: string;
+  refreshToken: string;
+}
+const {userEntitiesRef, withUserEntities} = entitiesPropsFactory('user');
+const {addressEntitiesRef, withAddressEntities} = entitiesPropsFactory('address');
 interface User{}
 //criando o estado a ser utilizado na nossa store
 const {state, config} = createState(
-  withProps<UserProps>({name:'',email:'', id:null}), //setando os valores iniciais dos campos
-  withEntities()
+  withProps<GlobalProps>({accessToken: "", refreshToken:""}), //setando propriedades globais ao state da minha store
+  withEntities<User, "id">({idKey: "id"}), //setando propriedades especificas da minha entidade usuario ao state da minha store
+  withAddressEntities<Address>()
 )
 
 const userStore = new Store({name: "user", state, config})
 
 
+userStore.subscribe(u => (console.log(u)));
